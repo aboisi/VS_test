0774c1a90857e955eacd9f37d9e73f8b090848cf
aboisi <149761398+aboisi@users.noreply.github.com>
2024-03-14 20:38:39 +0900

    細く説明追加

--- MyDiary.java
+++ MyDiary.java
@@ -1,7 +1,8 @@
 public class MyDiary {
     public static void main(String[] args) {
-        int version;
-        version = 1 + 5;
+        int version;//①変数宣言の分
+        version = 1 + 5;//②計算の文
+        // ③命令実行の分
         System.out.println("日記ソフト");
         System.out.println("ver" + version);
         System.out.println("まだ開発中です");
