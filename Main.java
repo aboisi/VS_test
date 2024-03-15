57838a248894dc74cbc85c951a8680a7f639f1fe
aboisi <149761398+aboisi@users.noreply.github.com>
2024-03-15 16:35:37 +0900

    練習1-2

--- Main.java
+++ Main.java
@@ -79,5 +79,11 @@ public class Main {
         name1 = "すがわら";//文字列データは(")で囲む
         System.out.println(name1);
 
+        //練習1-2
+        int a = 3;
+        int b = 5;
+        int c = a * b;
+        System.out.println("縦幅" + a + "横幅" + b + "の長方形の面積は、" + c);
+
     }
 }
