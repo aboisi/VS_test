4b58be05b3361fc048bd080a2596c60292495d2b
aboisi <149761398+aboisi@users.noreply.github.com>
2024-03-19 20:51:17 +0900

    配列作成

---
+++ Array.java
@@ -0,0 +1,9 @@
+public class Array {
+    public static void main(String[] args) {
+        //配列の長さを調べる
+        int[] scores = new int[5];
+        int num = scores.length; // 配列の要素数の取得[配列変数名.length]
+        System.out.println("要素の数:" + num);
+    }
+    
+}
