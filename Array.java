7b5d64716948b0edfa0f465f8bb930af0e9cd05d
aboisi <149761398+aboisi@users.noreply.github.com>
2024-03-19 21:48:59 +0900

    DNAの記号をランダム表示

--- Array.java
+++ Array.java
@@ -33,6 +33,39 @@ public class Array {
             }
         }
         System.out.println("50点以上の科目の数は" + count);
+
+        //DNAの記号をランダムに表示する
+        int[] seq = new int[10];
+        //塩基配列をランダムに生成
+        for (int i = 0; i < seq.length; i++) {
+            seq[i] = new java.util.Random().nextInt(4);
+        }
+        //生成した塩基配列の記号を表示
+        for (int i = 0; i < seq.length; i++) {
+            // switch(seq[i]) {
+            //     case 0 -> {
+            //         System.out.print("A ");
+            //     }
+            //     case 1 -> {
+            //         System.out.print("T ");
+            //     }
+            //     case 2 -> {
+            //         System.out.print("G ");
+            //     }
+            //     case 3 -> {
+            //         System.out.print("C ");
+            //     }
+            // }
+
+            //上記のswitchを3行に
+            char[] base = {'A', 'T', 'G', 'C'};
+            // int baseType = seq[i];//①
+            // char baseChar = base[baseType];//②
+            // System.out.print(baseChar + " ");//③
+            //上記の①~③を1行に
+            System.out.print(base[seq[i]] + " ");
+        }
+        
     }
     
 }
