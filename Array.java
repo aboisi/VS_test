a592dd006dff93c8e4fa3494b3843960a9ad6cc1
aboisi <149761398+aboisi@users.noreply.github.com>
2024-03-19 21:21:58 +0900

    点数管理プログラム

--- Array.java
+++ Array.java
@@ -12,6 +12,19 @@ public class Array {
         int[] scores2 = {20, 30, 40, 50, 80};
         System.out.println(scores1[1]);
         System.out.println(scores2[2]);
+        //配列とfor文
+        for (int i = 0; i < scores1.length; i++) {
+            System.out.println(scores1[i]);
+        }
+        //点数管理プログラム
+        int[] scores3 = {20, 30, 40, 50, 80}; //科目の数や点数を変更したい場合はこの行のみ修正
+        int sum = 0;
+        for (int i = 0; i < scores3.length; i++ ) {
+            sum += scores3[i];
+        }
+        int avg = sum / scores.length;
+        System.out.println("合計点" + sum);
+        System.out.println("平均点" + avg);
     }
     
 }
