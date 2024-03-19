a65eaaf3da427cc96b7fa772fa1f960cd5597921
aboisi <149761398+aboisi@users.noreply.github.com>
2024-03-19 20:55:05 +0900

    配列の要素に値を代入

--- Array.java
+++ Array.java
@@ -4,6 +4,9 @@ public class Array {
         int[] scores = new int[5];
         int num = scores.length; // 配列の要素数の取得[配列変数名.length]
         System.out.println("要素の数:" + num);
+        //配列の要素に値を代入
+        scores[1] =30;
+        System.out.println(scores[1]); 
     }
     
 }
