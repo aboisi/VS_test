3ce2d71c8f948621659e6bfefcccc7f8e0d59189
aboisi <149761398+aboisi@users.noreply.github.com>
2024-03-19 21:02:29 +0900

    省略記法

--- Array.java
+++ Array.java
@@ -7,6 +7,11 @@ public class Array {
         //配列の要素に値を代入
         scores[1] =30;
         System.out.println(scores[1]); 
+        //省略記法
+        int[] scores1 = new int[] {20, 30,40, 50, 80};
+        int[] scores2 = {20, 30, 40, 50, 80};
+        System.out.println(scores1[1]);
+        System.out.println(scores2[2]);
     }
     
 }
