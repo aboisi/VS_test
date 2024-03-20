0d8cf83f6432f802f7b624e185f196df5c9f05ea
aboisi <149761398+aboisi@users.noreply.github.com>
2024-03-20 16:02:48 +0900

    複数の配列で参照

--- Array.java
+++ Array.java
@@ -70,7 +70,12 @@ public class Array {
         for (int value : scores4) {
             System.out.println(value);
         }
-        
+        //複数の変数で参照
+        int[] arrayA = {1, 2, 3}; //arrayA[0]は1
+        int[] arrayB;
+        arrayB = arrayA;
+        arrayB[0] = 100; //arrayB[0]に100を代入
+        System.out.println(arrayA[0]); 
     }
     
 }
