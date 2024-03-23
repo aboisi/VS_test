26541112ac09bf214bb73e2f8848f367ffff037f
aboisi <149761398+aboisi@users.noreply.github.com>
2024-03-23 19:47:35 +0900

    練習4_3

---
+++ Practice4_3.java
@@ -0,0 +1,9 @@
+public class Practice4_3 {
+    public static void main(String[] args) {
+        int[] counts = null;
+        float[] heights = {173.3F, 175.0F};
+        System.out.println(counts[1]);//nullのため
+        System.out.println(heights[2]);//箱の数より指定している数字が大きい
+    }
+    
+}
