ad15a29d01700a7071c0734b78dfd06adf9b3540
aboisi <149761398+aboisi@users.noreply.github.com>
2024-03-23 18:16:54 +0900

    2次元の配列

--- Array.java
+++ Array.java
@@ -86,6 +86,18 @@ public class Array {
         //String型の後に.lengthを付けると、文字列の長さを取得
         String str = "Javaで開発";
         System.out.println(str.length());
+
+        //多次元の配列
+        //2次元配列
+        int[][] score = new int[2][3];
+        score[0][0] = 40;
+        score[0][1] = 50;
+        score[0][2] = 60;
+        score[1][0] = 80;
+        score[1][1] = 60;
+        score[1][2] = 70;
+        System.out.println(score[1][1]);
+
     }
     
 }
