3b8bff07411c558b659e8a4298e62a0b0b2ddf35
aboisi <149761398+aboisi@users.noreply.github.com>
2024-03-24 15:30:18 +0900

    戻り値の例

---
+++ MethodReturn.java
@@ -0,0 +1,11 @@
+public class MethodReturn {
+    public static int add(int x, int y) {
+        int ans = x + y;
+        return ans;
+    }
+    public static void main(String[] args) {
+        int ans = add(100, 10);//addメソッドの呼び出し
+        System.out.println("100 + 10 = " + ans);
+    }
+    
+}
