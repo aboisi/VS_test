1fd6872a9c24ff986275dae2195f6fe6a25b45c1
aboisi <149761398+aboisi@users.noreply.github.com>
2024-03-25 15:06:48 +0900

    戻り値に配列を用いる

---
+++ MethodArray3.java
@@ -0,0 +1,15 @@
+public class MethodArray3 {
+    public static int[] makeArray(int size) { //int型配列を作成して戻すメソッド
+        int[] newArray = new int[size];
+        for (int i = 0; i < newArray.length; i++) {
+            newArray[i] = i;
+        }
+        return newArray; //配列を戻す
+    }
+    public static void main(String[] args) {
+        int[] array = makeArray(3); //makeArrayがnewArrayになる
+        for (int i : array) { //arrayの全要素を出力
+            System.out.println(i);
+        }
+    }
+}
