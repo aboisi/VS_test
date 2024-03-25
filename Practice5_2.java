515fa23d20e2fa705397cad2e3e0d6f64b8ced4f
aboisi <149761398+aboisi@users.noreply.github.com>
2024-03-25 19:01:40 +0900

    練習5_3

--- Practice5_2.java
+++ Practice5_2.java
@@ -5,11 +5,17 @@ public class Practice5_2 {
         String address = "メールの宛先アドレス";
         String text = "メールの本文";
         email(title, address, text);
+        email(address, text);//練習5_3
     }
     public static void email(String title, String address, String text) {
         System.out.println(address + "に、以下のメールを送信しました");
         System.out.println("件名：" + title);
         System.out.println("本文" + text);
     }
-    
+    //練習5_3
+    public static void email(String address, String text) {
+        System.out.println(address + "に、以下のメールを送信しました");
+        System.out.println("件名：無題");
+        System.out.println("本文" + text);
+    }
 }
