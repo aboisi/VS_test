b37c927b2834be6a32543539eb59099d16690a92
aboisi <149761398+aboisi@users.noreply.github.com>
2024-03-28 15:38:58 +0900

    パッケージ、FQCN、import

--- Calc.java
+++ Calc.java
@@ -1,8 +1,10 @@
+package calcapp.main;
+import calcapp.logics.CalcLogic;//FQCN入力の手間を軽減
 public class Calc {
     public static void main(String[] args) {
         int a = 10; int b = 2;
-        int total = CalcLogic.tasu(a, b);
-        int delta = CalcLogic.hiku(a, b);
+        int total = CalcLogic.tasu(a, b);//FQCNでなくてもエラーにならない
+        int delta = calcapp.logics.CalcLogic.hiku(a, b);//FQCNを指定してもよい
         System.out.println("足すと" + total + "、引くと" + delta);
     }
     
