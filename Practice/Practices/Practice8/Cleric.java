0f741d9aa3c58da49a7460f47ef5a2f4e6131de2
aboisi <149761398+aboisi@users.noreply.github.com>
2024-04-04 19:49:35 +0900

    練習8-4

--- Practice/Practices/Practice8/Cleric.java
+++ Practice/Practices/Practice8/Cleric.java
@@ -1,3 +1,5 @@
+import java.util.*;
+
 public class Cleric {
     String name;
     int hp = 50;
@@ -10,5 +12,19 @@ public class Cleric {
         this.hp = this.MAX_HP;
         this.mp -= 5;
         System.out.println("HPが最大まで回復した");
+    }
+
+    public void pray(int sec) {
+        System.out.println(this.name + "は" + sec + "秒間天に祈った！");
+
+        //理論上の回復量を乱数を用いて決定する
+        int recover = new Random().nextInt(3) + sec;//祈った秒数にランダムで0~2を加える
+
+        //実際の回復量を計算する
+        int recoverActual = Math.min(this.MAX_MP - this.mp, recover);//MAX_MPから現在のMPを引いた値とrecoverを比べて小さい方を代入する
+
+        this.mp += recoverActual;
+        System.out.println("MPが" + recoverActual + "回復した");
+        return recoverActual;
     }
 }
