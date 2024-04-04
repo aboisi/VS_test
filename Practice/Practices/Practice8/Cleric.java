e014c54c6dce0bc3b0c74257a050604229b58ab3
aboisi <149761398+aboisi@users.noreply.github.com>
2024-04-04 19:21:57 +0900

    練習8-3 メソッド追加

--- Practice/Practices/Practice8/Cleric.java
+++ Practice/Practices/Practice8/Cleric.java
@@ -4,4 +4,11 @@ public class Cleric {
     final int MAX_HP = 50;
     int mp = 10;
     final int MAX_MP = 10;
+
+    public void selfAid() { //MPを5消費すると自分自身のHPを最大まで回復する
+        System.out.println(this.name + "はセルフエイドを唱えた！");
+        this.hp = this.MAX_HP;
+        this.mp -= 5;
+        System.out.println("HPが最大まで回復した");
+    }
 }
