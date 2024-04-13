fdc6d406a2e863eb850930d5f64a3b2e451d0b9c
aboisi <149761398+aboisi@users.noreply.github.com>
2024-04-13 20:21:24 +0900

    1〜10までの数字を偶数のみ表示

---
+++ test1.java
@@ -0,0 +1,12 @@
+public class test1 {
+    public static void main(String[] args) {
+        //1から10までの数字をループ
+        for (int i = 1; i <= 10; i++) {
+            //偶数かどうかを判断
+            if (i % 2 == 0) {
+                //偶数なら表示
+                System.out.println(i);
+            }
+        }
+    }
+}
