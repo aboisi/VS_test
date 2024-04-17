930897a8992ac17141882effe1606b47f00b837d
aboisi <149761398+aboisi@users.noreply.github.com>
2024-04-17 21:23:52 +0900

    末尾のカンマを非表示

--- test4.java
+++ test4.java
@@ -1,5 +1,6 @@
 public class test4 {
     public static void main(String[] args) {
+        String str = "";
         //要素数20の配列を宣言
         int[] numbers = new int[20];
         numbers[0] = 0;
@@ -10,23 +11,20 @@ public class test4 {
         }
         
         System.out.print("奇数：");
-        for(int i = 0; i < 11; i ++) {
-            if (numbers[i] % 2 != 0) {
-                System.out.print(numbers[i]);
-                if (i < numbers.length) {
-                    System.out.print(",");
-                }
+        for(int i = 0; i < 11; i ++) { //[0]~[10]の値を取り出す
+            if (numbers[i] % 2 != 0) { //奇数は真
+                    str += numbers[i] + ",";
             }
         }
+        System.out.print(str.substring(0, str.length() - 1));
 
+        str = "";
         System.out.print("偶数：");
         for(int i = 11; i < 20; i++) {
             if (numbers[i] % 2 == 0) {
-                System.out.print(numbers[i]);
-                if (i != 10) {
-                    System.out.print(",");
-                }
+                str += numbers[i] + ",";
             }
         }
+        System.out.print(str.substring(0, str.length() - 1));
     }
 }
