a2ab02f2500bea3f00da9659c67362db0dd06904
aboisi <149761398+aboisi@users.noreply.github.com>
2024-04-19 20:37:56 +0900

    範囲外の時再入力

--- test8.java
+++ test8.java
@@ -3,10 +3,24 @@
 //  負の数も含む。負の数の場合は、数値の直前に空白を挟まずに「-」（マイナス）を付加して入力するものとし、数値の範囲は-10000～10000とする。
 public class test8 {
     public static void main(String[] args) {
-        System.out.print("整数を入力してください");
-        int a = new java.util.Scanner(System.in).nextInt();
-        System.out.print("整数を入力してください");
-        int b = new java.util.Scanner(System.in).nextInt();
+        int a;
+        int b;
+        do {
+            System.out.print("-10000～10000の範囲の整数を入力してください。");
+            a = new java.util.Scanner(System.in).nextInt();
+            if (a < -10000 || a > 10000) {
+                System.out.println("範囲外です。再度入力してください。");
+            }
+        } while (a < -10000 || a > 10000);
+        
+
+        do {
+            System.out.print("-10000～10000の範囲の整数を入力してください。");
+            b = new java.util.Scanner(System.in).nextInt();
+            if (b < -10000 || b > 10000) {
+                System.out.println("範囲外です。再度入力してください。");
+            }
+        } while (b < -10000 || b > 10000);
         System.out.println(a + b);
     }
 }
